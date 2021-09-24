2e7adeea1b1c0d35988795a7990a7dd1594a36a0
khoirurroziqin17 <khoirurroziqin951@gmail.com>
2021-09-24 09:32:37 +0700

    fix max-h image

--- components/Cards/FeaturedCard.js
+++ components/Cards/FeaturedCard.js
@@ -3,13 +3,13 @@ import FeaturedCaption from "@components/Cards/FeaturedCaption";
 export default function FeaturedCard({ featured }) {
   return (
     <header
-      className="md:flex md:justify-between md:space-x-8 shadow-md md:shadow-none rounded-lg"
+      className="md:flex justify-between md:space-x-8 shadow-md md:shadow-none rounded-lg"
       key={featured.id}
     >
-      <a href={featured.link} className="md:w-7/12">
+      <a href={featured.link} className="md:w-7/12 md:flex md:justify-center">
         <img
           src={featured.thumbnail.url}
-          className={`rounded-t-lg md:rounded-xl ${
+          className={`rounded-t-lg md:rounded-xl md:max-h-80 ${
             featured.thumbnail.mime == "image/png" && "bg-yellow-300 p-10"
           }`}
           alt={featured.thumbnail.name}
